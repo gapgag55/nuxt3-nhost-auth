bdabbc4381f53ea9567fdb09c20f625e98a3392d
Kopkap <imkopkap.dev@gmail.com>
2023-03-12 01:25:46 +0700

    Remove: creds

--- services/nhost.ts
+++ services/nhost.ts
@@ -1,8 +1,8 @@
 import { NhostClient } from '@nhost/vue'
 
 const nhost = new NhostClient({
-  subdomain: 'klwlgekgfbmpcnxnkpir',
-  region: 'ap-southeast-1'
+  subdomain: '',
+  region: ''
 })
 
 export const auth = nhost.auth
